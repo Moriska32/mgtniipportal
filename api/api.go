4980e3f3d48cf9a5587c10edb07af4110b9abf0d
Верниковский Иван Юрьевич <Vernikovsky_IY@mgtniip.ru>
2020-07-30 09:56:41 +0300

    weather

--- api/api.go
+++ api/api.go
@@ -3,7 +3,6 @@ package api
 import (
 	config "PortalMGTNIIP/config"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 
@@ -197,20 +196,14 @@ type Valute struct {
 //Weather get Weather
 func Weather(c *gin.Context) {
 
-	url := "https://community-open-weather-map.p.rapidapi.com/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&mode=xml%252C%20html&q=London%252Cuk"
+	url := "https://gridforecast.com/api/v1/forecast/49.8479;35.6541/202007301200?api_token=fi83J3miGOyofI5D"
 
 	req, _ := http.NewRequest("GET", url, nil)
-
-	req.Header.Add("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
-	req.Header.Add("x-rapidapi-key", "7e8e2a23afmshf2deabcb8c7b3dfp177c0cjsne0b1f5d1b74e")
 
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": http.StatusOK,
