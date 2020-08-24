5f09d5b4b5d755354ef263802738e54be7380408
Верниковский Иван Юрьевич <Vernikovsky_IY@mgtniip.ru>
2020-08-24 09:07:07 +0300

    Meeting room

--- api/api.go
+++ api/api.go
@@ -420,8 +420,8 @@ func Weathers(c *gin.Context) {
 func Meetingroom(c *gin.Context) {
 
 	dbConnect := config.Connect()
-	todo := `SELECT object_id,"number"
-	FROM public.tobject where "number" in (2,9,505);`
+	todo := `SELECT *
+	FROM public.tobject where "number" in ('2','9','505') and type_id = 4;`
 
 	theCase := "lower"
 	data, err := gosqljson.QueryDbToMap(dbConnect, theCase, todo)
