9283488ed6b5a69967ddfcf889ae98b58b669263
Верниковский Иван Юрьевич <Vernikovsky_IY@mgtniip.ru>
2020-12-18 11:35:16 +0300

    training topics

--- routes/routes.go
+++ routes/routes.go
@@ -156,7 +156,7 @@ func Routes(router *gin.Engine) {
 			//TrainingAnakitic
 			root.GET("/getpooltrainingbyyear", training.Getpooltrainingbyyear)
 			root.GET("/getpoolusersbydep", training.Getpoolusersbydep)
-			root.GET("/getexelanaliticstraining", training.GetExelAnaliticsTraining)
+			root.GET("/getexcelanaliticstraining", training.GetExelAnaliticsTraining)
 
 			//TrainingRequest
 
